0dd7e0ee823ed91ce4464d81dd71e5b6b8ecf818
Noor Ul Ain Afaq <noorulain.afaqkhatri@gmail.com>
2025-10-23 18:49:05 +0500

    CapBoundingBox

--- Cap detection/CapBoundingBox.py
+++ Cap detection/CapBoundingBox.py
@@ -1,39 +1,29 @@
 import cv2
-import numpy as np
 
-img = np.zeros((200,200,3))
-
-cv2.circle(img,(20,20),10,(0,255,0),-1)
-
+image = cv2.imread("1.JPG")
+h, w, c = image.shape
+output = image.copy()
 
 
-def bbox(img):
-    
-    for y in range(0,200):
-        for x in range(0,200):
-            img2=img.copy()
-            
-            cv2.rectangle(img2,(x,y),(x+4,y+4),(0,255,255),-1)
-            if(img[y,x][1]==255):
-                
-                while(True):
-                    if(img[y,x][1]==255):
-                        y+=1
-                        print(y)
-                    else:
-                        break
-                
-                cv2.rectangle(img2,(0,y),(x+1,y),(255,0,255),-1)
-                
-                #cv2.rectangle(img2,(x,y),(x+4,y+4),(255,0,255),-1)
-                cv2.imshow("n",img2)
-                cv2.waitKey(0)
-                break
-            cv2.imshow("n",img2)
-            cv2.waitKey(1)
+xmin, ymin = w, h
+xmax, ymax = 0, 0
 
 
-bbox(img)
+for y in range(h):
+    for x in range(w):
+        b, g, r = image[y, x]
 
-cv2.imshow("n",img)
+        if r > 100 and r > 1.5 * g and r > 1.3 * b:
+            if x < xmin: xmin = x
+            if x > xmax: xmax = x
+            if y < ymin: ymin = y
+            if y > ymax: ymax = y
+        else:
+           
+            output[y, x] = [0, 0, 0]
+
+cv2.rectangle(output, (xmin, ymin), (xmax, ymax), (255, 255, 255), 2)
+
+cv2.imshow("Cap Bounding Box", output)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
